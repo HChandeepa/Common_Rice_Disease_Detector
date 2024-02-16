0968a15a6079b26be92828179bd2b1adef5af30d
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2024-02-16 16:06:14 +0530

    API model

--- API/main.py
+++ API/main.py
@@ -1,17 +1,28 @@
+import numpy as np
 from fastapi import FastAPI,File,UploadFile
 import uvicorn
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
+
+MODEL=tf.keras.models.load_model("../saved models/1")
+CLASS_NAMES=["BrownSpot","Healthy","Hispa","LeafBlast"]
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
 
+def read_file_as_image(data) -> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
 @app.post("/predict")
 async def predict(
        file:UploadFile=File(...)
 ):
-    bytes= file.read()
+    image= read_file_as_image(await file.read())
+
 
     return
 
