02475f1f93bc3ba01d7715b5e1f879200489a2fe
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2024-02-17 13:34:37 +0530

    removed

--- API/main.py
+++ API/main.py
@@ -1,30 +1,13 @@
-import numpy as np
-from fastapi import FastAPI,File,UploadFile
+from fastapi import FastAPI
 import uvicorn
-from io import BytesIO
-from PIL import Image
-import tensorflow as tf
-
-app = FastAPI()
-
-MODEL=tf.keras.models.load_model("../saved models/1")
-CLASS_NAMES=["BrownSpot","Healthy","Hispa","LeafBlast"]
+app=FastAPI()
 
 @app.get("/ping")
 async def ping():
-    return "Hello, I am alive"
+    return "Hello,I am alive"
 
-def read_file_as_image(data) -> np.ndarray:
-    image=np.array(Image.open(BytesIO(data)))
-    return image
-@app.post("/predict")
-async def predict(
-       file:UploadFile=File(...)
-):
-    image= read_file_as_image(await file.read())
+@app.post("/ping")
+async def ping():
 
-
-    return
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+if __name__=="__main__":
+    uvicorn.run(app,host='localhost',port=8000)
