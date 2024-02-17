88d56eb5e174dbb254ce9167c424b4cf6fb7a88f
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2024-02-17 16:30:02 +0530

    chnages

--- API/main.py
+++ API/main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+import numpy as np
+from fastapi import FastAPI,File,UploadFile
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 app=FastAPI()
+
+MODEL=tf.keras.models.load_model("../saved models/1")
+CLASS_NAMES = ["BrownSpot", "Healthy", "Hispa","LeafBlast"]
 
 @app.get("/ping")
 async def ping():
     return "Hello,I am alive"
 
-@app.post("/ping")
-async def ping():
+def read_file_as_image(data)-> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+       file:UploadFile=File(...)
+):
+    image=read_file_as_image(await file.read())
+
+    img_batch = np.expand_dims(image, 0)
+
+    prediction=MODEL.predict(img_batch)
+    pass
 
 if __name__=="__main__":
     uvicorn.run(app,host='localhost',port=8000)
